87ca1fd4cf56d1e7897e32d9b348d76d60d8847e
Santiago-Farias <santifa8@gmail.com>
2024-12-04 08:57:27 -0300

    feat: se modificaron comentarios y se agregaron las preguntas y respuestas y el ranking de jugadores a datos.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame.mixer as mixer
 from funciones import *
 from datos import *
 import os
+from datos import *
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 WIN_PATH = os.path.join(BASE_DIR, "win.mp3")
@@ -22,132 +23,6 @@ jugador_nombre = ""  # Almacena el texto ingresado
 DIFICULTAD_FACIL = 1
 DIFICULTAD_MEDIA = 3
 DIFICULTAD_DIFICIL = 5
-
-preguntas_respuestas = [
-  {
-    "pregunta": "¿Quién es el máximo goleador histórico de la selección argentina?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Gabriel Batistuta" },
-      { "opcion": "B", "texto": "Lionel Messi" },
-      { "opcion": "C", "texto": "Sergio Agüero" },
-      { "opcion": "D", "texto": "Diego Maradona" }
-    ],
-    "respuesta_correcta": "B",
-    "dificultad": "F"
-  },
-  {
-    "pregunta": "¿En qué año Argentina ganó su primer Mundial de Fútbol?",
-    "respuestas": [
-      { "opcion": "A", "texto": "1962" },
-      { "opcion": "B", "texto": "1978" },
-      { "opcion": "C", "texto": "1986" },
-      { "opcion": "D", "texto": "1930" }
-    ],
-    "respuesta_correcta": "B",
-    "dificultad": "M"
-  },
-  {
-    "pregunta": "¿Qué club argentino ha ganado más títulos de la Copa Libertadores?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Boca Juniors" },
-      { "opcion": "B", "texto": "River Plate" },
-      { "opcion": "C", "texto": "Independiente" },
-      { "opcion": "D", "texto": "Racing Club" }
-    ],
-    "respuesta_correcta": "C",
-    "dificultad": "F"
-  },
-  {
-    "pregunta": "¿Quién fue el técnico de la selección argentina durante la Copa del Mundo de 1986?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Carlos Bilardo" },
-      { "opcion": "B", "texto": "César Luis Menotti" },
-      { "opcion": "C", "texto": "Alfio Basile" },
-      { "opcion": "D", "texto": "Daniel Passarella" }
-    ],
-    "respuesta_correcta": "A",
-    "dificultad": "D"
-  },
-  {
-    "pregunta": "¿Qué jugador argentino jugó en el Barcelona y el Real Madrid?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Javier Saviola" },
-      { "opcion": "B", "texto": "Gonzalo Higuaín" },
-      { "opcion": "C", "texto": "Ángel Di María" },
-      { "opcion": "D", "texto": "Claudio Caniggia" }
-    ],
-    "respuesta_correcta": "A",
-    "dificultad": "M"
-  },
-  {
-    "pregunta": "¿En qué año Diego Maradona anotó el famoso 'Gol del Siglo' contra Inglaterra?",
-    "respuestas": [
-      { "opcion": "A", "texto": "1986" },
-      { "opcion": "B", "texto": "1982" },
-      { "opcion": "C", "texto": "1990" },
-      { "opcion": "D", "texto": "1994" }
-    ],
-    "respuesta_correcta": "A",
-    "dificultad": "M"
-  },
-  {
-    "pregunta": "¿Quién es el entrenador de la selección argentina en el Mundial de 2022?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Jorge Sampaoli" },
-      { "opcion": "B", "texto": "Alejandro Sabella" },
-      { "opcion": "C", "texto": "Lionel Scaloni" },
-      { "opcion": "D", "texto": "Ricardo Gareca" }
-    ],
-    "respuesta_correcta": "C",
-    "dificultad": "D"
-  },
-  {
-    "pregunta": "¿Qué equipo argentino fue el primero en ganar la Copa Intercontinental?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Independiente" },
-      { "opcion": "B", "texto": "Boca Juniors" },
-      { "opcion": "C", "texto": "River Plate" },
-      { "opcion": "D", "texto": "Racing Club" }
-    ],
-    "respuesta_correcta": "D",
-    "dificultad": "F"
-  },
-  {
-    "pregunta": "¿En qué estadio se jugó la final del Mundial de 1978, donde Argentina se consagró campeón?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Estadio Monumental" },
-      { "opcion": "B", "texto": "Estadio de La Bombonera" },
-      { "opcion": "C", "texto": "Estadio José María Minella" },
-      { "opcion": "D", "texto": "Estadio Antonio Vespucio Liberti" }
-    ],
-    "respuesta_correcta": "A",
-    "dificultad": "D"
-  },
-  {
-    "pregunta": "¿Qué jugador argentino es conocido como 'El Pibe de Oro'?",
-    "respuestas": [
-      { "opcion": "A", "texto": "Carlos Tevez" },
-      { "opcion": "B", "texto": "Sergio Agüero" },
-      { "opcion": "C", "texto": "Diego Maradona" },
-      { "opcion": "D", "texto": "Juan Román Riquelme" }
-    ],
-    "respuesta_correcta": "C",
-    "dificultad": "D"
-  }
-]
-
-jugadores_ranking = [
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0],
-    ["", 0, 0]
-]
 
 ranking_ordenado = None
 
@@ -172,8 +47,6 @@ sonido_win.set_volume(0.3)
 
 sonido_lose = mixer.Sound(LOSE_PATH)
 sonido_lose.set_volume(0.1)
-
-texto_a_mostrar = "falopa"
 
 current_screen = "menu principal"
 
@@ -265,10 +138,10 @@ while True:
                 jugador_vidas = 3
                 jugador_puntos = 0
                 current_screen = "menu principal"
-                jugador_nombre = ""  # Limpia el texto después de confirmar
-            elif evento.key == pygame.K_BACKSPACE:  # Eliminar último carácter
+                jugador_nombre = ""
+            elif evento.key == pygame.K_BACKSPACE:
                 jugador_nombre = jugador_nombre[:-1]
-            else:  # Agregar el carácter presionado
+            else:
                 jugador_nombre += evento.unicode
         
     
@@ -277,19 +150,19 @@ while True:
     elif current_screen == "pregunta":
         recuadros_opciones = draw_pregunta_screen(preguntas_respuestas, indice_pregunta_actual, pantalla, boton_rect_salir, fuente)
         tiempo_actual = time.time()
-        tiempo_restante = max(0, 6 - (tiempo_actual - tiempo_inicial))  # Tiempo máximo es 5 segundos
+        tiempo_restante = max(0, 6 - (tiempo_actual - tiempo_inicial))  # 5 segundos para responder.
 
-        # Dibuja las opciones de la pregunta y el tiempo restante
+        # Dibujar la pregunta y el tiempo restante.
         recuadros_opciones = draw_pregunta_screen(preguntas_respuestas, indice_pregunta_actual, pantalla, boton_rect_salir, fuente)
         draw_time_left(pantalla, fuente, tiempo_restante)
 
-        # Si el tiempo se acaba, pasa automáticamente a la siguiente pregunta
+        # Pasar a la otra pregunta si el tiempo se acaba.
         if tiempo_restante == 0:
             sonido_lose.play()
             jugador_vidas -= 1  # Penalización por no responder
             indice_pregunta_actual += 1
 
-            # Reinicia o vuelve al menú principal si ya no hay preguntas
+            # Volver al menú principal o reiniciar si no hay más preguntas.
             if indice_pregunta_actual >= len(preguntas_respuestas) or jugador_vidas == 0:
                 current_screen = "menu principal"
                 indice_pregunta_actual = 0
@@ -309,7 +182,7 @@ while True:
         else:
             draw_ranking_screen(jugadores_ranking, pantalla, boton_rect_salir, fuente)
 
-    # Efecto hover para los botones.
+    # Agregar efecto hover para los botones.
     if current_screen == "menu principal":
         if (mouse[0] >= 325 and mouse[0] <= 475) and (mouse[1] >= 600 and mouse[1] <= 650):
             dibujar_boton(WHITE2, GREY1, pantalla, boton_rect_salir, "Salir", fuente)
